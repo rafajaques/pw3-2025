2aff80941137c8b7fc4578f545f275f03c2f0099
Rafael Jaques <rafael@phpit.com.br>
2025-07-10 11:30:40 -0300

    Aula 16 - Factories (pt_BR), Faker e Tinker

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -16,5 +16,7 @@ class CategorySeeder extends Seeder
         Category::create(['name' => 'Variedades']);
         Category::create(['name' => 'Economia']);
         Category::create(['name' => 'Lazer']);
+        Category::create(['name' => 'Esportes']);
+        Category::create(['name' => 'Política']);
     }
 }
